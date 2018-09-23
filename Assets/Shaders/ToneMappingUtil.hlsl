fdd8e733ceeb8e9ed15791acf7bf284b9960c241
Parikshit Saraswat <psaraswa@usc.edu>
2018-09-23 05:02:38 -0700

    Added the division by linear white point value.

--- Assets/Shaders/ToneMappingUtil.hlsl
+++ Assets/Shaders/ToneMappingUtil.hlsl
@@ -5,8 +5,9 @@
 #define D (0.20f)
 #define E (0.02f)
 #define F (0.30f)
+#define LINEAR_WHITE_POINT_VALUE (float3(11.2f, 11.2f, 11.2f))
 
 inline float3 Uncharted2Tonemap(float3 x)
 {
-	return TONEMAP_FUNCTION(x);
+	return TONEMAP_FUNCTION(x)/TONEMAP_FUNCTION(LINEAR_WHITE_POINT_VALUE);
 }
